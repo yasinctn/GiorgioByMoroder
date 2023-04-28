208dc9880e842a5ad5ff965120b653cf33b3056a
Yasin <yasincetin2014@gmail.com>
2023-04-28 17:46:13 +0300

    initial commit

---
+++ Giorgio/GiorgioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiorgioApp.swift
+//  Giorgio
+//
+//  Created by Yasin Cetin on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GiorgioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
